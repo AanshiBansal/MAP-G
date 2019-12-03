58cd77e1111bf4a5c3e4d3500a6ed97d9f669446
Shreya <shreyagrg09@gmail.com>
2019-12-03 15:19:22 +0530

    adding closing modals

--- src/Components/BalloonBlow.js
+++ src/Components/BalloonBlow.js
@@ -3,15 +3,22 @@ import 'semantic-ui-css/semantic.min.css';
 import {Button, Modal, Header, Icon, Card} from 'semantic-ui-react'
 import ReactStopwatch from "react-stopwatch";
 import Balloon from "./Balloon";
+import {Redirect} from "react-router-dom";
 
 export default class BalloonBlow extends Component{
     state = {
         modalOpen: false,
-        stopWatch:false
+        stopWatch:false,
+        modalOpe:false,
+        redirecthome: false
     };
 
     handleOpen = () => this.setState({ modalOpen: true });
     handleClose = () => this.setState({ modalOpen: false,stopWatch:true });
+    openClose = () => this.setState({ modalOpe: true });
+    closeClose = () => {
+        this.setState({ redirecthome: true });
+    };
     //updateTime = (seconds) => this.seconds=seconds;
 
     componentDidMount() {
@@ -53,10 +60,10 @@ export default class BalloonBlow extends Component{
                     seconds={0}
                     minutes={0}
                     hours={0}
-                    limit="00:00:60"
+                    limit="00:00:30"
                     onChange={({hours, minutes, seconds}) => {
                     }}
-                    onCallback={() => console.log('Finish')}
+                    onCallback={() => this.openClose()}
                     render={({formatted, hours, minutes, seconds}) => {
                         return (
                             <div style={{float:'right'}}>
@@ -65,6 +72,29 @@ export default class BalloonBlow extends Component{
                         );
                     }}
                 />
+                <Modal
+                    //trigger={<Button onClick={this.handleOpen}>Show Modal</Button>}
+                    open={this.state.modalOpe}
+                    onClose={this.closeClose}
+                    basic
+                    size='small'
+                >
+                    <Header icon='browser' content='Instructions'/>
+                    <Modal.Content>
+                        <h3>
+                            The goal of this game is to accumulate as much as money possible by pumping balloons.
+                            Each balloon has an explosion point, where too much pumping pops the balloon.
+                            To pump a balloon, click Pump.
+                            Each pump earns $ 5, but can only be earned if balloon is collected by clicking Collect before the balloon explodes.
+                        </h3>
+                    </Modal.Content>
+                    <Modal.Actions>
+                        <Button color='green' onClick={this.closeClose} inverted>
+                            <Icon name='checkmark'/> Got it
+                        </Button>
+                    </Modal.Actions>
+                </Modal>
+                { (this.state.redirecthome) ? <Redirect to="/"/> : null }
                 <Balloon />
             </div>
         )
--- src/Components/ListeningGame.js
+++ src/Components/ListeningGame.js
@@ -2,15 +2,23 @@ import React, {Component} from 'react';
 import {Button, Header,Icon, Modal, Card} from 'semantic-ui-react'
 import ReactStopwatch from 'react-stopwatch';
 import Game2 from "./Game2";
+import {Redirect} from "react-router-dom";
 
 export default class ListeningGame extends Component{
     state = {
         modalOpen: false,
-        stopWatch:false
+        stopWatch:false,
+        modalope: false,
+        redirecthome: false
     };
+
 
     handleOpen = () => this.setState({ modalOpen: true });
     handleClose = () => this.setState({ modalOpen: false,stopWatch:true });
+    openClose = () => this.setState({ modalOpe: true });
+    closeClose = (event) => {
+        this.setState({ redirecthome: true });
+    };
 
     componentDidMount() {
         this.handleOpen=this.handleOpen.bind(this);
@@ -51,7 +59,7 @@ export default class ListeningGame extends Component{
                     limit="00:02:00"
                     onChange={({hours, minutes, seconds}) => {
                     }}
-                    onCallback={() => console.log('Finish')}
+                    onCallback={() => this.openClose()}
                     render={({formatted, hours, minutes, seconds}) => {
                         const sec = 60*minutes + seconds;
                         return (
@@ -61,6 +69,29 @@ export default class ListeningGame extends Component{
                         );
                     }}
                 />
+                <Modal
+                    //trigger={<Button onClick={this.handleOpen}>Show Modal</Button>}
+                    open={this.state.modalOpe}
+                    onClose={this.closeClose}
+                    basic
+                    size='small'
+                >
+                    <Header icon='browser' content='Instructions'/>
+                    <Modal.Content>
+                        <h3>
+                            The goal of this game is to accumulate as much as money possible by pumping balloons.
+                            Each balloon has an explosion point, where too much pumping pops the balloon.
+                            To pump a balloon, click Pump.
+                            Each pump earns $ 5, but can only be earned if balloon is collected by clicking Collect before the balloon explodes.
+                        </h3>
+                    </Modal.Content>
+                    <Modal.Actions>
+                        <Button color='green' onClick={this.closeClose} inverted>
+                            <Icon name='checkmark'/> Got it
+                        </Button>
+                    </Modal.Actions>
+                </Modal>
+                { (this.state.redirecthome) ? <Redirect to="/"/> : null }
 
                 <Game2 />
             </div>
