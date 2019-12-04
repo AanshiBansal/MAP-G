438658ba7e151deb7f1ab764b4fdfd6e2893ea4d
Aanshi <aanshi.bansal@gmail.com>
2019-12-04 10:11:58 +0530

    Link disable

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,6 +3,17 @@ import { Card } from 'semantic-ui-react'
 import { Link } from 'react-router-dom';
 
 class Home extends Component {
+    state={
+        disable:[]
+    };
+    handleClick=(event)=>{
+        const id = event.target.id;
+        const data =[];
+        for(let i=0;i<this.state.disable.length;i++)
+            data.push(this.state.disable[i]);
+        data[id]="";
+        this.setState({disable:data});
+    };
     render() {
         return (
             <div>
@@ -19,22 +30,22 @@ class Home extends Component {
                             {/*</Card>*/}
                             <Card>
                                 <Card.Content>
-                                    <Link to="/listening">Sharpness in Listening</Link>
+                                    <Link to="/listening" id='1' onClick={this.handleClick}>Sharpness in Listening</Link>
                                 </Card.Content>
                             </Card>
                             <Card>
                                 <Card.Content>
-                                    <Link to="/pile-choosing">Earn Maximum</Link>
+                                    <Link to="/pile-choosing" id='2' onClick={this.handleClick}>Earn Maximum</Link>
                                 </Card.Content>
                             </Card>
                             <Card>
                                 <Card.Content>
-                                    <Link to="/balloon-blow">Blow the Balloon</Link>
+                                    <Link to="/balloon-blow" id='3' onClick={this.handleClick}>Blow the Balloon</Link>
                                 </Card.Content>
                             </Card>
                             <Card>
                                 <Card.Content>
-                                    <Link to="/emotion-sensing">Emotion Predictor</Link>
+                                    <Link to="/emotion-sensing" id='4' onClick={this.handleClick}>Emotion Predictor</Link>
 
                                 </Card.Content>
                             </Card>
