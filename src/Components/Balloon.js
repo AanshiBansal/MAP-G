8b624eae1f4b9ba963bd1c893afcc9f362c7f9a2
Aanshi <aanshi.bansal@gmail.com>
2020-03-20 15:08:53 +0530

    Removed typos

--- src/Components/Balloon.js
+++ src/Components/Balloon.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import {Button, Card, Image} from "semantic-ui-react";
 import { connect } from 'react-redux';
 import blue from '../Content/blue.png';
-import blueblast from '../Content/blueblast.png';
+import blueBlast from '../Content/blueblast.png';
 import red from '../Content/red.png';
-import redblast from '../Content/redblast.png';
+import redBlast from '../Content/redblast.png';
 import yellow from '../Content/yellow.png';
-import yelloBlst from '../Content/yellowBlst.png';
+import yellowBlast from '../Content/yellowBlst.png';
 import balloonPump from '../Content/balloonPump.jpg';
 import {balloonClicked} from "../Actions";
 
@@ -23,8 +23,8 @@ class Balloon extends Component{
         total:0,
         src:red
     };
-    imgsrc=[[red,blue,yellow],[redblast,blueblast,yelloBlst]];
-    imgsize=['mini','tiny','small','medium','large','big'];
+    imgSrc=[[red,blue,yellow],[redBlast,blueBlast,yellowBlast]];
+    imgSize=['mini','tiny','small','medium','large','big'];
     val=[[3,4,4,3,4,3,3,4,3,4,4,3],[5,6,6,5,6,5,5,6,5,6,6,5],[2,3,2,2,3,2,2,3,2,2,2,3]];
     clickCount=0;
     clickLimit=3;
@@ -37,7 +37,7 @@ class Balloon extends Component{
 
             //display blast for 2 secs
             // eslint-disable-next-line
-           // console.log("Balloon bursted after "+this.clickCount +" clicks without collecting the potential amount"+ " at " + new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(timestamp));
+           // console.log("Balloon busted after "+this.clickCount +" clicks without collecting the potential amount"+ " at " + new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(timestamp));
             this.props.balloonClicked({
                 emailId: this.props.userInfo.email,
                 color: this.state.balloonColour,
@@ -57,24 +57,24 @@ class Balloon extends Component{
             counter[this.state.balloonColour]=counter[this.state.balloonColour]+1;
             this.clickCount=0;
             this.clickLimit=this.val[balloonColour][counter[balloonColour]];
-           // document.querySelector('balloon').src = this.imgsrc[1][this.state.balloonColour];
-            document.getElementById("balloon").src = this.imgsrc[1][this.state.balloonColour];
+           // document.querySelector('balloon').src = this.imgSrc[1][this.state.balloonColour];
+            document.getElementById("balloon").src = this.imgSrc[1][this.state.balloonColour];
             setTimeout(() => {
-                this.setState({balloonColour:balloonColour,count:this.state.count+1,src:this.imgsrc[0][balloonColour],Balloon:data,countBalloon:counter,size:0});
+                this.setState({balloonColour:balloonColour,count:this.state.count+1,src:this.imgSrc[0][balloonColour],Balloon:data,countBalloon:counter,size:0});
             }, 500);
 
         }
     };
     handleCollect=()=>{
         // eslint-disable-next-line
-        const timestamp = Date.now();
+        //const timestamp = Date.now();
         // eslint-disable-next-line
-        console.log("After "+this.clickCount +" clicks the potential amount was collected" + " at " + new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(timestamp));
+        //console.log("After "+this.clickCount +" clicks the potential amount was collected" + " at " + new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(timestamp));
         const balloonColour=(this.state.balloonColour+1)%3;
         const data = [];
         for(let i=0;i<this.state.Balloon.length;i++)
             data.push(this.state.Balloon[i]);
-        data.push([this.state.count,this.state.balloonColour,this.clickCount,this.clickLimit]);
+        data.push([this.state.count, this.state.balloonColour, this.clickCount, this.clickLimit]);
 
         const counter=[];
         for(let i=0;i<3;i++)
@@ -91,7 +91,7 @@ class Balloon extends Component{
             score:this.score,
             total: current
         });
-        this.setState({balloonColour:balloonColour,count:this.state.count+1,src:this.imgsrc[0][balloonColour],Balloon:data,countBalloon:counter,total:current,size:0});
+        this.setState({balloonColour:balloonColour,count:this.state.count+1,src:this.imgSrc[0][balloonColour],Balloon:data,countBalloon:counter,total:current,size:0});
 
     };
 
@@ -106,7 +106,7 @@ class Balloon extends Component{
                     <Button onClick={this.handleCollect}>Collect</Button>
                 </div>
                 <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
-                    <Image src={this.state.src} id ='balloon' size = {this.imgsize[this.state.size]} onClick={this.handleClick}/>
+                    <Image src={this.state.src} id ='balloon' size = {this.imgSize[this.state.size]} onClick={this.handleClick}/>
                 </div><div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
                     <Image src={balloonPump} size='small'/>
 
